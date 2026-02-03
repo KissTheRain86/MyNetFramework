10668dfcc9efeded3185714ff54c78f04dec3c87
kisstherain8677 <842658636@qq.com>
2026-02-04 02:03:17 +0800

    完善

--- Assets/Scripts/ByteArray.cs
+++ Assets/Scripts/ByteArray.cs
@@ -10,7 +10,7 @@ public class ByteArray
     //初始大小
     int initSize = 0;
     //缓冲区
-    public byte[] Bytes {  get; private set; }
+    private byte[] _bytes;
     //读写位置
     public int ReadIndex = 0;
     public int WriteIndex = 0;
@@ -23,7 +23,7 @@ public class ByteArray
 
     public ByteArray(int size = DEFAULT_SIZE)
     {
-        Bytes = new byte[size];
+        _bytes = new byte[size];
         capacity = size;
         initSize = size;
         ReadIndex = 0;
@@ -32,7 +32,7 @@ public class ByteArray
 
     public ByteArray(byte[] defaultBytes)
     {
-        Bytes = defaultBytes;
+        _bytes = defaultBytes;
         capacity = defaultBytes.Length;
         initSize = defaultBytes.Length;
         ReadIndex = 0;
@@ -47,8 +47,60 @@ public class ByteArray
         while (n < size) n *= 2;
         capacity = n;
         byte[] newBytes = new byte[capacity];
-        Array.Copy(Bytes, ReadIndex, newBytes, 0, WriteIndex - ReadIndex);
-        Bytes = newBytes;
+        Array.Copy(_bytes, ReadIndex, newBytes, 0, WriteIndex - ReadIndex);
+        _bytes = newBytes;
+        WriteIndex = Length;
+        ReadIndex = 0;
+    }
+
+    //写入数据
+    public int Write(byte[] bs,int offset,int count)
+    {
+        if (Remain < count) Resize(Length + count);
+        Array.Copy(bs, offset, _bytes, WriteIndex, count);
+        WriteIndex += count;
+        return count;
+    }
+
+    //读取数据
+    public int Read(byte[] bs,int offset,int count)
+    {
+        count = Math.Min(count, Length);
+        Array.Copy(_bytes, 0, bs, offset, count);
+        ReadIndex += count;
+        CheckAndMoveBytes();
+        return count;
+    }
+
+    public Int16 ReadInt16()
+    {
+        if (Length < 2) return 0;
+        Int16 ret = (Int16)((_bytes[1] << 8) | _bytes[0]);
+        ReadIndex += 2;
+        CheckAndMoveBytes();
+        return ret;
+    }
+
+    public Int32 ReadInt32()
+    {
+        if(Length<4) return 0;
+        Int32 ret = (Int32)((_bytes[3]<<24)|
+            (_bytes[2]<<16)|
+            (_bytes[1]<<8)|
+            _bytes[0]);
+        ReadIndex += 4;
+        CheckAndMoveBytes();
+        return ret;
+    }
+    private void CheckAndMoveBytes()
+    {
+        if (Length < 8) MoveBytes();
+    }
+
+    private void MoveBytes()
+    {
+        //移到最前
+        Array.Copy(_bytes, ReadIndex, _bytes, 0, Length);
         WriteIndex = Length;
         ReadIndex = 0;
     }
