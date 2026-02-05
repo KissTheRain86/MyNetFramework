0c6601fe7532669228606f75bf9ce282e1e64969
zengweihao <zwh86771385@gmail.com>
2026-02-05 18:20:24 +0800

    数据发送

--- Assets/Scripts/Net/Framework/ByteArray.cs
+++ Assets/Scripts/Net/Framework/ByteArray.cs
@@ -12,7 +12,7 @@ namespace ZNet
         //初始大小
         int initSize = 0;
         //缓冲区
-        private byte[] _bytes;
+        public byte[] Bytes { get; private set; }
         //读写位置
         public int ReadIndex = 0;
         public int WriteIndex = 0;
@@ -25,7 +25,7 @@ namespace ZNet
 
         public ByteArray(int size = DEFAULT_SIZE)
         {
-            _bytes = new byte[size];
+            Bytes = new byte[size];
             capacity = size;
             initSize = size;
             ReadIndex = 0;
@@ -34,7 +34,7 @@ namespace ZNet
 
         public ByteArray(byte[] defaultBytes)
         {
-            _bytes = defaultBytes;
+            Bytes = defaultBytes;
             capacity = defaultBytes.Length;
             initSize = defaultBytes.Length;
             ReadIndex = 0;
@@ -49,8 +49,8 @@ namespace ZNet
             while (n < size) n *= 2;
             capacity = n;
             byte[] newBytes = new byte[capacity];
-            Array.Copy(_bytes, ReadIndex, newBytes, 0, WriteIndex - ReadIndex);
-            _bytes = newBytes;
+            Array.Copy(Bytes, ReadIndex, newBytes, 0, WriteIndex - ReadIndex);
+            Bytes = newBytes;
             WriteIndex = Length;
             ReadIndex = 0;
         }
@@ -59,7 +59,7 @@ namespace ZNet
         public int Write(byte[] bs, int offset, int count)
         {
             if (Remain < count) Resize(Length + count);
-            Array.Copy(bs, offset, _bytes, WriteIndex, count);
+            Array.Copy(bs, offset, Bytes, WriteIndex, count);
             WriteIndex += count;
             return count;
         }
@@ -68,7 +68,7 @@ namespace ZNet
         public int Read(byte[] bs, int offset, int count)
         {
             count = Math.Min(count, Length);
-            Array.Copy(_bytes, 0, bs, offset, count);
+            Array.Copy(Bytes, 0, bs, offset, count);
             ReadIndex += count;
             CheckAndMoveBytes();
             return count;
@@ -77,7 +77,7 @@ namespace ZNet
         public Int16 ReadInt16()
         {
             if (Length < 2) return 0;
-            Int16 ret = (Int16)((_bytes[1] << 8) | _bytes[0]);
+            Int16 ret = (Int16)((Bytes[1] << 8) | Bytes[0]);
             ReadIndex += 2;
             CheckAndMoveBytes();
             return ret;
@@ -86,10 +86,10 @@ namespace ZNet
         public Int32 ReadInt32()
         {
             if (Length < 4) return 0;
-            Int32 ret = (Int32)((_bytes[3] << 24) |
-                (_bytes[2] << 16) |
-                (_bytes[1] << 8) |
-                _bytes[0]);
+            Int32 ret = (Int32)((Bytes[3] << 24) |
+                (Bytes[2] << 16) |
+                (Bytes[1] << 8) |
+                Bytes[0]);
             ReadIndex += 4;
             CheckAndMoveBytes();
             return ret;
@@ -102,7 +102,7 @@ namespace ZNet
         private void MoveBytes()
         {
             //移到最前
-            Array.Copy(_bytes, ReadIndex, _bytes, 0, Length);
+            Array.Copy(Bytes, ReadIndex, Bytes, 0, Length);
             WriteIndex = Length;
             ReadIndex = 0;
         }
--- Assets/Scripts/Net/Framework/NetManager.cs
+++ Assets/Scripts/Net/Framework/NetManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
+using ProtoBuf;
+using System.Linq;
 
 namespace ZNet
 {
@@ -68,6 +70,28 @@ namespace ZNet
             }
         }
 
+        public static void Send(IExtensible msg)
+        {
+            if (socket == null || !socket.Connected) return;
+            if (isConnecting || isClosing) return;
+            //数据编码
+            var sendBytes = Encode(msg);
+            //写入队列
+            ByteArray ba = new ByteArray(sendBytes);
+            int count = 0;//writequeue的长度
+            lock (writeQueue)
+            {
+                writeQueue.Enqueue(ba);
+                count = writeQueue.Count;
+            }
+            //发送
+            if (count == 1)
+            {
+                socket.BeginSend(sendBytes, 0, sendBytes.Length,
+                    0, SendCallback, socket);
+            }
+        }
+
         private static void ConnectCallback(IAsyncResult ar)
         {
             try
@@ -87,25 +111,59 @@ namespace ZNet
             }
         }
 
+        private static void SendCallback(IAsyncResult ar)
+        {
+            Socket socket = (Socket)(ar.AsyncState);
+            if (socket == null || !socket.Connected) return;
+            //返回实际发送成功的字节数 可能没有把当期消息全部发送完
+            int count = socket.EndSend(ar);
+            //获取当期正在发送的bytearray
+            ByteArray ba = null;
+            lock (writeQueue)
+            {
+                if (writeQueue.Count > 0)
+                {
+                    ba = writeQueue.First();
+                    //记录 刚才发送了count个字节
+                    ba.ReadIndex += count;
+                    if (ba.Length == 0)
+                    {
+                        //判断当期协议是否发送完
+                        writeQueue.Dequeue();
+                        ba = writeQueue.Count>0? writeQueue.First():null;
+                    }
+                }
+            }
+            //循环发送
+            if (ba != null)
+            {
+                socket.BeginSend(ba.Bytes, ba.ReadIndex, ba.Length,
+                    0, SendCallback, socket);
+            }
+            else if (isClosing)
+            {
+                socket.Close();
+            }
+        }
 
         #region encode and decode
 
-        public static byte[] Encode(ProtoBuf.IExtensible msg)
+        public static byte[] Encode(IExtensible msg)
         {
             using(var memory = new System.IO.MemoryStream())
             {
-                ProtoBuf.Serializer.Serialize(memory, msg);
+                Serializer.Serialize(memory, msg);
                 return memory.ToArray();
             }
         }
 
-        public static ProtoBuf.IExtensible Decode(string protoName,
+        public static IExtensible Decode(string protoName,
             byte[] bytes,int offset,int count)
         {
             using(var memory = new System.IO.MemoryStream(bytes, offset, count))
             {
                 Type t = Type.GetType(protoName);
-                return (ProtoBuf.IExtensible)ProtoBuf.Serializer.NonGeneric.Deserialize(t, memory);
+                return (IExtensible)Serializer.NonGeneric.Deserialize(t, memory);
             }
         }
         #endregion
